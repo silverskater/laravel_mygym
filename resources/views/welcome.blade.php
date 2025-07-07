72e8f600d839bdc07327b4f785ecef17e81ced1e
Anton Ludescher <ludescherantal@gmail.com>
2025-07-07 16:11:23 +0300

    feat: Add background image to the welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen">
-            <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                <div class="flex flex-col justify-center items-center">
-                    <h1 class="font-light text-8xl">MyGym</h1>
-                    <div class="mt-8">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+<div class="relative min-h-screen bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
+    <div class="absolute inset-0 bg-white/90"></div>
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen">
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="flex flex-col justify-center items-center">
+                <h1 class="font-light text-8xl">MyGym</h1>
+                <div class="mt-8">
                     @if (Route::has('login'))
                         @auth
                             <a href="{{ route('dashboard') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150">Dashboard</a>
@@ -21,9 +23,10 @@
                             <a href="{{ route('login') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150">Log In</a>
                         @endauth
                     @endif
-                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
