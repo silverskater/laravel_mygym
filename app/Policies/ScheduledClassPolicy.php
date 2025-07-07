46053800c3421a48f6f6bf74d409ac9088210155
Anton Ludescher <ludescherantal@gmail.com>
2025-07-07 12:00:16 +0300

    feat: Allow scheduled class deletion only if it has >2 hours before start.

--- app/Policies/ScheduledClassPolicy.php
+++ app/Policies/ScheduledClassPolicy.php
@@ -10,6 +10,8 @@ class ScheduledClassPolicy
     public function delete(User $user, ScheduledClass $scheduledClass)
     {
         // Check if the user is admin or the instructor of the scheduled class.
-        return $user->id === $scheduledClass->instructor_id || $user->role === 'admin';
+        return ($user->id === $scheduledClass->instructor_id || $user->role === 'admin')
+            //&& $scheduledClass->scheduled_at->isFuture()
+            && $scheduledClass->scheduled_at > now()->addHours(2);
     }
 }
